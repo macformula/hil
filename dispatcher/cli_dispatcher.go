a246de0615024e4a64c3c2f19d86647bf216bffe
Hady Ibrahim <hadyib007@gmail.com>
2023-12-29 23:51:54 -0500

    dispatcher cli wip

--- dispatcher/cli_dispatcher.go
+++ dispatcher/cli_dispatcher.go
@@ -2,58 +2,66 @@ package dispatcher
 
 import (
 	"context"
-	"github.com/macformula/hil/flow"
+	"github.com/google/uuid"
 	"github.com/macformula/hil/orchestrator"
 )
 
-type cliDispatcher struct {
-	start             chan flow.Sequence
-	quit              chan struct{}
-	recoverFromFatal  chan struct{}
-	progress          chan flow.Progress
-	orchestratorState orchestrator.State
-	stop              chan struct{}
+type CliDispatcher struct {
+	start            chan orchestrator.StartSignal
+	results          chan orchestrator.ResultSignal
+	status           chan orchestrator.StatusSignal
+	cancelTest       chan orchestrator.TestId
+	recoverFromFatal chan struct{}
+	testsToRun       uuid.UUID
+	stop             chan struct{}
+	cli              cliInterface
 }
 
-func newCliDispatcher() *cliDispatcher {
-	return &cliDispatcher{
-		start:    make(chan flow.Sequence),
-		quit:     make(chan struct{}),
-		progress: make(chan flow.Progress),
+func NewCliDispatcher() *CliDispatcher {
+	return &CliDispatcher{
+		start:      make(chan orchestrator.StartSignal, 5),
+		results:    make(chan orchestrator.ResultSignal),
+		status:     make(chan orchestrator.StatusSignal),
+		cancelTest: make(chan orchestrator.TestId),
+		//testsToRun: make([]uuid.UUID, 1), // keep it to 1
+		cli: newCli(),
 	}
 }
 
-func (c *cliDispatcher) Close() error {
+func (c *CliDispatcher) Close() error {
+	err := c.cli.Close()
+	return err
+}
+
+func (c *CliDispatcher) Open(ctx context.Context) error {
+	err := c.cli.Open(ctx)
+	if err != nil {
+		return err
+	}
+
+	c.cli.Start()
+
+	return nil
+}
+
+func (c *CliDispatcher) Start() <-chan orchestrator.StartSignal {
+	return c.start
+}
+
+func (c *CliDispatcher) CancelTest() <-chan orchestrator.TestId {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (c *cliDispatcher) Open(ctx context.Context) error {
+func (c *CliDispatcher) RecoverFromFatal() <-chan struct{} {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (c *cliDispatcher) Start() <-chan orchestrator.StartSignal {
-	//TODO implement me
-	panic("implement me")
+func (c *CliDispatcher) Status() chan<- orchestrator.StatusSignal {
+	return c.status
 }
 
-func (c *cliDispatcher) CancelTest() <-chan orchestrator.TestId {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (c *cliDispatcher) RecoverFromFatal() <-chan struct{} {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (c *cliDispatcher) Status() chan<- orchestrator.StatusSignal {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (c *cliDispatcher) Results() chan<- orchestrator.ResultSignal {
-	//TODO implement me
-	panic("implement me")
+func (c *CliDispatcher) Results() chan<- orchestrator.ResultSignal {
+	return c.results
 }
---
+++ dispatcher/cli_proxy.go
@@ -0,0 +1,98 @@
+package dispatcher
+
+import (
+	"context"
+	"fmt"
+	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbles/spinner"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/macformula/hil/flow"
+	"github.com/macformula/hil/orchestrator"
+	"io"
+	"os"
+)
+
+type cliInterface interface {
+	io.Closer
+	Open(context.Context) error
+	Start() chan orchestrator.StartSignal
+	CancelTest() chan orchestrator.TestId
+	Status() chan orchestrator.StatusSignal
+	RecoverFromFatal() chan struct{}
+}
+
+func newCli() *model {
+	items := []list.Item{
+		item{title: "All Tests", desc: "Run all Test Suites"},
+		item{title: "AMK Test Suite", desc: "Runs all tests regarding the motor"},
+		item{title: "BMS Test Suite", desc: "Runs all tests regarding the battery"},
+	}
+
+	status := orchestrator.StatusSignal{
+		OrchestratorState: orchestrator.Running,
+		TestId:            orchestrator.TestId{},
+		Progress:          flow.Progress{},
+		QueueLength:       0,
+		FatalError:        nil,
+	}
+
+	const showLastResults = 5
+
+	sp := spinner.New()
+	sp.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("206"))
+	llist := list.New(items, list.NewDefaultDelegate(), 0, 0)
+	llist.Title = "HIL"
+
+	model := model{
+		list:        llist,
+		startChan:   make(chan orchestrator.StartSignal),
+		resultsChan: make(chan orchestrator.ResultSignal),
+		statusChan:  make(chan orchestrator.StatusSignal),
+		status:      status,
+		spinner:     sp,
+		results:     make([]result, showLastResults),
+	}
+
+	return &model
+}
+
+func (c model) Close() error {
+	c.Quitting = true
+	fmt.Println("quitting")
+	return nil
+}
+
+func (c model) Open(ctx context.Context) error {
+	go c.run()
+	return nil
+}
+
+func (c model) Start() chan orchestrator.StartSignal {
+	return c.startChan
+}
+
+func (c model) run() {
+	f, _ := tea.LogToFile("debug.log", "debug")
+	defer f.Close()
+
+	p := tea.NewProgram(c, tea.WithAltScreen())
+	c.program = p
+
+	if _, err := p.Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
+}
+
+func (c model) CancelTest() chan orchestrator.TestId {
+	return c.cancelChan
+}
+
+func (c model) RecoverFromFatal() chan struct{} {
+	return c.recoverChan
+}
+
+func (c model) Status() chan orchestrator.StatusSignal {
+	return c.statusChan
+}
