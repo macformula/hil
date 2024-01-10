81d8b35ca060b8b648c45081e3c9341570b5a2b6
Samuel Parent <108418256+samparent97@users.noreply.github.com>
2024-01-10 12:06:41 -0500

    Result processor gRPC client implementation (#39)
    * Created gRPC client module
    
    * Updated and refactored generated gRPC files
    
    * Implemented ResultProcessorIface on client
    
    * Added constructor and better error returns
    
    ---------
    
    Author: Tyler St-Amour <tylerstamour1@gmail.com>

---
+++ results/client/client.go
@@ -0,0 +1,81 @@
+package client
+
+import (
+	"context"
+	"github.com/google/uuid"
+	proto "github.com/macformula/hil/results/client/generated"
+	"github.com/pkg/errors"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type ResultsClient struct {
+	addr   string
+	conn   *grpc.ClientConn
+	client proto.TagTunnelClient
+}
+
+func NewResultsClient(ip, port string) *ResultsClient {
+	return &ResultsClient{
+		addr: ip + ":" + port,
+	}
+}
+
+func (r *ResultsClient) Open(ctx context.Context) error {
+	conn, err := grpc.DialContext(ctx, r.addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return errors.Wrap(err, "dial context")
+	}
+	r.conn = conn
+	r.client = proto.NewTagTunnelClient(conn)
+	return nil
+}
+
+func (r *ResultsClient) SubmitTag(ctx context.Context, tag string, value any) (bool, error) {
+	request, err := createRequest(tag, value)
+	if err != nil {
+		return false, errors.Wrap(err, "create request")
+	}
+
+	reply, err := r.client.SubmitTag(ctx, request)
+	if err != nil {
+		if !reply.Success {
+			return false, errors.New(reply.Error)
+		}
+		return reply.IsPassing, errors.Wrap(err, "submit tag")
+	}
+	return reply.IsPassing, nil
+}
+
+func (r *ResultsClient) CompleteTest(ctx context.Context, testId uuid.UUID) (bool, error) {
+	reply, err := r.client.CompleteTest(ctx, &proto.CompleteTestRequest{TestId: testId.String()})
+	if err != nil {
+		return reply.TestPassed, errors.Wrap(err, "complete test")
+	}
+	return reply.TestPassed, nil
+}
+
+func (r *ResultsClient) SubmitError(ctx context.Context, err error) error {
+	_, submitErr := r.client.SubmitError(ctx, &proto.SubmitErrorRequest{Error: err.Error()})
+	if submitErr != nil {
+		return errors.Wrap(err, "submit error")
+	}
+	return nil
+}
+
+func createRequest(tag string, data any) (*proto.SubmitTagRequest, error) {
+	request := &proto.SubmitTagRequest{Tag: tag}
+	switch data.(type) {
+	case int32:
+		request.Data = &proto.SubmitTagRequest_ValueInt{ValueInt: data.(int32)}
+	case float32:
+		request.Data = &proto.SubmitTagRequest_ValueFloat{ValueFloat: data.(float32)}
+	case string:
+		request.Data = &proto.SubmitTagRequest_ValueStr{ValueStr: data.(string)}
+	case bool:
+		request.Data = &proto.SubmitTagRequest_ValueBool{ValueBool: data.(bool)}
+	default:
+		return nil, errors.Errorf("unsupported data type for tag submission (%T)", data)
+	}
+	return request, nil
+}
