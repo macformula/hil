8e3e2e2b21b96c0927f074db03a0b3dd7e5e5c7a
Ivan Lange <langei@mcmaster.ca>
2024-01-12 15:23:29 -0500

    Add comments to analog and digital interfaces

--- iocontrol/analog.go
+++ iocontrol/analog.go
@@ -5,11 +5,13 @@ import (
 	"github.com/ethereum/go-ethereum/event"
 )
 
+// DataPoint contains relevant data to be transferred over a channel
 type DataPoint struct {
 	Value float64
 	err   error
 }
 
+// AnalogPin defines methods for analog pin control
 type AnalogPin interface {
 	SetDirection(context.Context, Direction) error
 	Read(context.Context) (float64, error)
--- iocontrol/digital.go
+++ iocontrol/digital.go
@@ -1,6 +1,8 @@
 package iocontrol
 
+// DigitalPin defines methods for digital pin control
 type DigitalPin interface {
+	Open() error
 	SetDirection(Direction) error
 	Read() (Level, error)
 	Write(Level) error
