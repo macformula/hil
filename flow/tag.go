1d62c98fa34585888256725d2f5e4694153bfbec
samparent97 <samuelparent97@gmail.com>
2024-01-12 16:04:15 -0500

    Cleanup

--- flow/tag.go
+++ flow/tag.go
@@ -1,6 +1,6 @@
 package flow
 
-// Tag is a way to identify expected bounds for a given result.
+// Tag is a way to identify expected bounds for a given result. These are submitted to a result processor.
 type Tag struct {
 	// ID is the tag identifier.
 	ID string `yaml:"tagId"`
