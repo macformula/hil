3e7e94b9b2229f93e40a7dff6bd7b73a356e0496
Tyler St-Amour <tylerstamour1@gmail.com>
2024-02-14 20:09:12 -0500

    Tracking for CAN frames by message type

--- canlink/can_client.go
+++ canlink/can_client.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net"
 
+	"github.com/pkg/errors"
 	"go.einride.tech/can"
 	"go.einride.tech/can/pkg/generated"
 	"go.einride.tech/can/pkg/socketcan"
@@ -18,18 +19,24 @@ type MessagesDescriptor interface {
 type CANClient struct {
 	md MessagesDescriptor
 	rx *socketcan.Receiver
+
+	// tracker keeps track of how many CAN messages have been received (per message type)
+	tracker    map[uint32]uint32
+	isTracking bool
+	stopSignal chan struct{}
 }
 
 // NewCANClient creates a new CANClient with socketcan connection.
 func NewCANClient(messages MessagesDescriptor, conn net.Conn) CANClient {
 	return CANClient{
-		md: messages,
-		rx: socketcan.NewReceiver(conn),
+		md:         messages,
+		rx:         socketcan.NewReceiver(conn),
+		isTracking: false,
 	}
 }
 
 // Read is a blocking function for reading a single CAN message. If given multiple possible message types to read, it
-// will return the first message received from those types.
+// will return the first message received from those types. If no types are given, it will return the first message available.
 func (c *CANClient) Read(ctx context.Context, msgsToRead ...generated.Message) (generated.Message, error) {
 	for {
 		select { // TODO: maybe implement a max timeout here just to be safe?
@@ -42,6 +49,15 @@ func (c *CANClient) Read(ctx context.Context, msgsToRead ...generated.Message) (
 			}
 
 			frame := c.rx.Frame()
+			// No message types were specified, return first frame of any type
+			if len(msgsToRead) == 0 {
+				msg, err := c.md.UnmarshalFrame(frame)
+				if err != nil {
+					return nil, err
+				}
+				return msg, nil
+			}
+
 			for _, msgToRead := range msgsToRead {
 				if frame.ID == msgToRead.Frame().ID {
 					msg, err := c.md.UnmarshalFrame(frame)
@@ -53,4 +69,38 @@ func (c *CANClient) Read(ctx context.Context, msgsToRead ...generated.Message) (
 			}
 		}
 	}
+}
+
+// StartTracking initiates the tracking goroutine. This is so we can check how many CAN frames of a certain type have
+// come through the CAN bus in a given time.
+func (c *CANClient) StartTracking() {
+	c.tracker = make(map[uint32]uint32)
+	c.stopSignal = make(chan struct{})
+	c.isTracking = true
+
+	go func(c *CANClient) {
+		for {
+			select {
+			case <-c.stopSignal:
+				c.isTracking = false
+				return
+			default:
+				msg, err := c.Read(nil, nil)
+				if err != nil { // TODO: maybe log these errors?
+					continue
+				}
+				c.tracker[msg.Frame().ID] += 1
+			}
+		}
+	}(c)
+}
+
+// StopTracking stops the tracker goroutine and returns the obtained frame counts.
+func (c *CANClient) StopTracking() (map[uint32]uint32, error) {
+	if !c.isTracking {
+		return nil, errors.New("CANClient tracker was never started")
+	}
+
+	close(c.stopSignal)
+	return c.tracker, nil
 }
