7f2065084525931c729708ad6105723eb0821112
Hady Ibrahim <hadyib007@gmail.com>
2024-03-02 16:12:59 -0500

    test removing server dispatcher

--- cmd/servertest/main.go
+++ cmd/servertest/main.go
@@ -4,12 +4,12 @@ import (
 	"context"
 	"github.com/macformula/hil/cli"
 	"github.com/macformula/hil/flow"
-	"github.com/macformula/hil/httpdispatcher"
 	"github.com/macformula/hil/orchestrator"
 	"github.com/macformula/hil/results/client"
 	"github.com/macformula/hil/test"
 	"github.com/pkg/errors"
 	"go.uber.org/zap"
+	"time"
 )
 
 const (
@@ -32,9 +32,9 @@ func main() {
 	resultProcessor := client.NewResultsClient(_resultProcessorIp, _resultProcessorPort, _pushToGithub)
 	sequencer := flow.NewSequencer(resultProcessor, logger)
 	cliDispatcher := cli.NewCliDispatcher(test.Sequences, logger)
-	//simpleDispatcher := test.NewSimpleDispatcher(logger, 5*time.Second, 10*time.Second)
-	server := httpdispatcher.NewServerDispatcher(test.Sequences, httpdispatcher.NewHttpServer(logger), logger)
-	o := orchestrator.NewOrchestrator(sequencer, logger, server, cliDispatcher) //cliDispatcher, simpleDispatcher)
+	simpleDispatcher := test.NewSimpleDispatcher(logger, 5*time.Second, 10*time.Second)
+	//server := httpdispatcher.NewServerDispatcher(test.Sequences, httpdispatcher.NewHttpServer(logger), logger)
+	o := orchestrator.NewOrchestrator(sequencer, logger, simpleDispatcher, cliDispatcher) //cliDispatcher, simpleDispatcher)
 
 	err = o.Open(context.Background())
 	if err != nil {
