c744b3c75233020cbb0d9ac41af4db126d1bd777
smartk14 <smartk1@mcmaster.ca>
2024-03-09 20:37:48 -0500

    change length

--- cmd/tracetest/main.go
+++ cmd/tracetest/main.go
@@ -69,7 +69,7 @@ func main() {
 	for i := 0; i < 15; i++ {
 		c := can.Frame{
 			ID:         1600,
-			Length:     0,
+			Length:     8,
 			Data:       can.Data{byte(i)},
 			IsRemote:   false,
 			IsExtended: false,
@@ -82,7 +82,7 @@ func main() {
 	for i := 0; i < 10; i++ {
 		c := can.Frame{
 			ID:         1600,
-			Length:     0,
+			Length:     8,
 			Data:       can.Data{byte(i)},
 			IsRemote:   false,
 			IsExtended: false,
