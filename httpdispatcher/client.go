f4bdb4a1d93db29b07a60ed6fe77841285f8f6ef
Tathagata Sikdar <rikisikdar1@gmail.com>
2024-03-14 01:35:01 -0400

    Refactored ws to 2 endpoints, added client methods

--- httpdispatcher/client.go
+++ httpdispatcher/client.go
@@ -1,21 +1,34 @@
 package httpdispatcher
 
 import (
+	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
 )
 
-// WebSocket client connection
-type Client struct {
-	Conn           *websocket.Conn // The WebSocket connection
-	Send           chan []byte     // Channel for outgoing messages
-	receiveHandler func([]byte)    // Handler for processing received messages
-	errchannel     chan error
+type TestQueueItem struct {
+	queueNumber int
+	UUID        uuid.UUID
 }
 
-func NewClient(conn *websocket.Conn, receiveHandler func([]byte)) *Client {
+type Client struct {
+	conn      *websocket.Conn
+	testQueue []TestQueueItem
+}
+
+func NewClient(conn *websocket.Conn) *Client {
 	return &Client{
-		Conn:           conn,
-		Send:           make(chan []byte, 256), // Buffered channel for outgoing messages
-		receiveHandler: receiveHandler,
+		conn: conn,
+		// testQueue: make(chan TestQueueItem, 10),
+		testQueue: make([]TestQueueItem, 0),
 	}
+}
+
+func (c *Client) addTestToQueue(conn *websocket.Conn, queueNumber int, testID uuid.UUID) {
+	c.conn = conn
+	//add queuenumber and testID to testQueue
+	newItem := TestQueueItem{
+		queueNumber: queueNumber,
+		UUID:        testID,
+	}
+	c.testQueue = append(c.testQueue, newItem)
 }
--- httpdispatcher/httpserver.go
+++ httpdispatcher/httpserver.go
@@ -8,11 +8,22 @@ import (
 	"strconv"
 
 	"github.com/google/uuid"
-
 	"github.com/gorilla/websocket"
 	"github.com/macformula/hil/flow"
 	"github.com/macformula/hil/orchestrator"
 	"go.uber.org/zap"
+)
+
+type Message struct {
+	task      string
+	parameter string
+}
+
+// message task values
+const (
+	StartTest        = "start"
+	CancelTest       = "cancel"
+	RecoverFromFatal = "recover"
 )
 
 type HttpServer struct {
@@ -118,232 +129,86 @@ func (h *HttpServer) createWS(w http.ResponseWriter, r *http.Request) *websocket
 }
 
 func (h *HttpServer) setupServer() {
-	http.HandleFunc("/status", h.serveStatus)
-	http.HandleFunc("/sequences", h.serveSequences)
-	http.HandleFunc("/start", h.serveStart) // results will be sent here
-	// http.HandleFunc("/cancel", serveStatus)
-	// http.HandleFunc("/recover", serveStatus)
+	http.HandleFunc("/test", h.serveTest) // start/sequences - cancel - recover
+	// http.HandleFunc("/sequences", h.serveSequences) - remove later
+	// http.HandleFunc("/status", h.serveStatus)
 }
 
-func (h *HttpServer) serveStatus(w http.ResponseWriter, r *http.Request) {
+func (h *HttpServer) serveTest(w http.ResponseWriter, r *http.Request) {
 	conn := h.createWS(w, r)
+	client := NewClient(conn)
 	defer conn.Close()
 
 	for {
-		messageType, message, err := conn.ReadMessage()
-		if err != nil {
-			h.l.Error("Read error", zap.Error(err))
-			break
-		}
-		// Log the received message
-		h.l.Info("Received message", zap.String("message", string(message)))
+		msg := h.readWS(conn)
 
-		statusJSON, err := json.Marshal(h.Status())
-		if err := conn.WriteMessage(messageType, statusJSON); err != nil {
+		switch msg.task {
+		case StartTest:
+			h.startTest(client, msg.parameter)
+		case CancelTest:
+			// h.cancelTest(client, cancelIndex)
+		case RecoverFromFatal:
+			// h.recoverTest()
+		}
+	}
+
+	//
+}
+
+func (h *HttpServer) readWS(conn *websocket.Conn) *Message {
+	_, message, err := conn.ReadMessage()
+	if err != nil {
+		h.l.Error("Read error", zap.Error(err))
+		return nil
+	}
+	var msg Message
+	if err := json.Unmarshal(message, &msg); err != nil {
+		h.l.Error("JSON Unmarshal error", zap.Error(err))
+		return nil
+	}
+	h.l.Info("Extracted values", zap.String("task", msg.task), zap.String("parameter", msg.parameter))
+	return &msg
+}
+
+func (h *HttpServer) startTest(client *Client, parameter string) {
+	var messageInt int
+	var err error
+
+	// If no parameter is provided, send the list of sequences to the client.
+	if parameter == "" {
+		sequencesJSON, _ := json.Marshal(h.sequences)
+		if err := client.conn.WriteMessage(websocket.TextMessage, sequencesJSON); err != nil {
 			h.l.Error("Write error", zap.Error(err))
 			return
 		}
-	}
-}
-
-func (h *HttpServer) serveStart(w http.ResponseWriter, r *http.Request) {
-	conn := h.createWS(w, r)
-	defer conn.Close()
-
-	for {
-		messageType, message, err := conn.ReadMessage()
-		if err != nil {
-			h.l.Error("Read error", zap.Error(err))
-			break
-		}
-
-		messageInt, err := strconv.Atoi(string(message))
+	} else {
+		// If a parameter is provided, convert it to an integer.
+		messageInt, err = strconv.Atoi(string(parameter))
 		if err != nil {
 			h.l.Error("The message is not an integer.", zap.Error(err))
-			break
-		}
-
-		// Log the received message
-		h.l.Info("Received message", zap.String("message", string(message)))
-		var httpCode string
-		if sequence, ok := h.sequences[messageInt]; ok {
-			// key exists in sequences map
-			h.start <- orchestrator.StartSignal{
-				TestId:   uuid.New(),
-				Seq:      sequence,
-				Metadata: nil,
-			}
-			httpCode = "204"
-		} else {
-			httpCode = "400"
-		}
-
-		// Echo the message back to the client
-		if err := conn.WriteMessage(messageType, []byte(httpCode)); err != nil {
-			h.l.Error("Write error", zap.Error(err))
 			return
 		}
 	}
-}
 
-func (h *HttpServer) serveSequences(w http.ResponseWriter, r *http.Request) {
-	conn := h.createWS(w, r)
-	defer conn.Close()
-
-	for {
-		messageType, message, err := conn.ReadMessage()
-		if err != nil {
-			h.l.Error("Read error", zap.Error(err))
-			break
+	var httpCode string
+	if sequence, ok := h.sequences[messageInt]; ok {
+		newTestID := uuid.New()
+		// Add test to client test list
+		queuePosition := (<-h.status).QueueLength
+		client.addTestToQueue(queuePosition, newTestID)
+		// Send start signal
+		h.start <- orchestrator.StartSignal{
+			TestId:   newTestID,
+			Seq:      sequence,
+			Metadata: nil,
 		}
-		// Log the received message
-		h.l.Info("Received message", zap.String("message", string(message)))
-
-		// send sequences
-		sequencesJSON, err := json.Marshal(h.sequences)
-		if err := conn.WriteMessage(messageType, sequencesJSON); err != nil {
-			h.l.Error("Write error", zap.Error(err))
-			return
-		}
+		httpCode = http.StatusText(http.StatusOK)
+	} else {
+		httpCode = http.StatusText(http.StatusBadRequest)
 	}
+	httpCodeJSON, _ := json.Marshal(httpCode)
+	client.conn.WriteMessage(websocket.TextMessage, httpCodeJSON)
 }
-
-// func (h *HttpServer) serveCancel(w http.ResponseWriter, r *http.Request) {
-// 	conn := h.createWS(w, r)
-
-// }
-
-// func (h *HttpServer) serverRecover(w http.ResponseWriter, r *http.Request) {
-// 	conn := h.createWS(w, r)
-
-// }
-
-// func (h *HttpServer) setupServer() error {
-// 	err := h.setupDispatcherStatusEndpoint()
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	err = h.setupSequencesEndpoint()
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	err = h.setupStartTestEndpoint()
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	return nil
-// }
-
-// Prepares the HTTP server to handle WebSocket upgrade requests
-// func (h *HttpServer) setupDispatcherStatusEndpoint() error {
-// 	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
-// 		conn := h.createWS(w, r)
-// 		defer conn.Close()
-
-// 		// Handle incoming messages in a separate goroutine
-// 		for {
-
-// 			messageType, message, err := conn.ReadMessage()
-// 			if err != nil {
-// 				h.l.Error("Read error", zap.Error(err))
-// 				break
-// 			}
-// 			// Log the received message
-// 			h.l.Info("Received message", zap.String("message", string(message)))
-// 			prefix := "Sending: "
-// 			msg := append([]byte(prefix), message...)
-// 			// Echo the message back to the client
-// 			if err := conn.WriteMessage(messageType, msg); err != nil {
-// 				h.l.Error("Write error", zap.Error(err))
-// 				return
-// 			}
-// 		}
-// 	})
-
-// 	return nil
-// }
-
-// func (h *HttpServer) setupSequencesEndpoint() error {
-// 	http.HandleFunc("/sequences", func(w http.ResponseWriter, r *http.Request) {
-// 		conn := h.createWS(w, r)
-// 		defer conn.Close()
-
-// 		// Handle incoming messages in a separate goroutine
-// 		for {
-
-// 			messageType, message, err := conn.ReadMessage()
-// 			if err != nil {
-// 				h.l.Error("Read error", zap.Error(err))
-// 				break
-// 			}
-// 			// Log the received message
-// 			h.l.Info("Received message", zap.String("message", string(message)))
-
-// 			jsonData, err := json.Marshal(h.sequences)
-
-// 			// Echo the message back to the client
-// 			if err := conn.WriteMessage(messageType, jsonData); err != nil {
-// 				h.l.Error("Write error", zap.Error(err))
-// 				return
-// 			}
-// 		}
-// 	})
-
-// 	return nil
-// }
-
-// func (h *HttpServer) setupStartTestEndpoint() error {
-// 	http.HandleFunc("/starttest", func(w http.ResponseWriter, r *http.Request) {
-// 		conn, err := upgrader.Upgrade(w, r, nil)
-// 		if err != nil {
-// 			h.l.Error("Failed to upgrade to websocket", zap.Error(err))
-// 			return
-// 		}
-// 		defer conn.Close()
-
-// 		// Handle incoming messages in a separate goroutine
-// 		for {
-
-// 			messageType, message, err := conn.ReadMessage()
-// 			if err != nil {
-// 				h.l.Error("Read error", zap.Error(err))
-// 				break
-// 			}
-
-// 			messageInt, err := strconv.Atoi(string(message))
-// 			if err != nil {
-// 				h.l.Error("The message is not an integer.", zap.Error(err))
-// 				break
-// 			}
-
-// 			// Log the received message
-// 			h.l.Info("Received message", zap.String("message", string(message)))
-// 			var httpCode string
-// 			if sequence, ok := h.sequences[messageInt]; ok {
-// 				// key exists in sequences map
-// 				h.start <- orchestrator.StartSignal{
-// 					TestId:   uuid.New(),
-// 					Seq:      sequence,
-// 					Metadata: nil,
-// 				}
-// 				httpCode = "204"
-// 			} else {
-// 				httpCode = "400"
-// 			}
-
-// 			// Echo the message back to the client
-// 			if err := conn.WriteMessage(messageType, []byte(httpCode)); err != nil {
-// 				h.l.Error("Write error", zap.Error(err))
-// 				return
-// 			}
-// 		}
-// 	})
-
-// 	return nil
-// }
 
 func (h *HttpServer) startServer() {
 	addr := ":8080"
