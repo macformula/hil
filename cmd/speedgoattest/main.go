a90724cbdd74f83adc1d09d408a7245e79050c36
Tyler St-Amour <tylerstamour1@gmail.com>
2024-04-03 11:18:46 -0400

    Refactor speedgoat test

--- cmd/speedgoattest/main.go
+++ cmd/speedgoattest/main.go
@@ -1,30 +1,49 @@
 package main
 
 import (
+	"time"
+
 	"github.com/macformula/hil/iocontrol/speedgoat"
 	"go.uber.org/zap"
-	"time"
 )
 
-func main() {
-	controller := speedgoat.NewController(zap.L(), "192.168.7.5:8001")
+const _speedgoatAddr = "192.168.7.5:8001"
 
-	err := controller.Open()
+func main() {
+	cfg := zap.NewDevelopmentConfig()
+	logger, err := cfg.Build()
 	if err != nil {
 		panic(err)
 	}
+	defer logger.Sync()
 
-	pin := speedgoat.NewDigitalPin(8)
+	controller := speedgoat.NewController(logger, _speedgoatAddr)
+
+	err = controller.Open()
+	if err != nil {
+		logger.Error("controller open", zap.Error(err))
+		return
+	}
+
+	pin := speedgoat.NewDigitalPin(8) // First digital output pin (idx 8-15)
 	controller.SetDigital(pin, true)
-	pin2 := speedgoat.NewDigitalPin(15)
+	pin2 := speedgoat.NewDigitalPin(15) // Last digital output pin
 	controller.SetDigital(pin2, true)
-	pin3 := speedgoat.NewAnalogPin(8)
-	controller.WriteVoltage(pin3, 1000)
+	pin3 := speedgoat.NewAnalogPin(8) // First analog output pin (idx 8-11)
+	controller.WriteVoltage(pin3, 2.5)
 
 	time.Sleep(time.Second * 2)
-	
+
+	// Verify that these change on the Speedgoat via Simulink or LED
+	// If they don't change (or take a while), verify that the Simulink TCP server has a sufficiently small sample time
 	controller.SetDigital(pin2, false)
 	controller.WriteVoltage(pin3, 3)
 
 	time.Sleep(time.Second * 2)
+
+	err = controller.Close()
+	if err != nil {
+		logger.Error("controller close", zap.Error(err))
+		return
+	}
 }
