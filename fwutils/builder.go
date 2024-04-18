d061635b70a50c02e3bf379623a22627eb8ccec7
Ivan Lange <langei@mcmaster.ca>
2024-04-17 22:41:30 -0400

    Finish build method

--- fwutils/builder.go
+++ fwutils/builder.go
@@ -18,9 +18,12 @@ const (
 	_makePlatformArg       = "PLATFORM="
 	_makeProjectArg        = "PROJECT="
 
-	_defaultBinaryName = "main.bin"
-	_buildFolder       = "/build"
-	_loggerName        = "builder"
+	_defaultBinaryName      = "main"
+	_defaultBinaryExtension = ".bin"
+	_buildFolder            = "build"
+	_loggerName             = "builder"
+
+	_platformStm32 = "stm32f767"
 )
 
 // Builder takes a firmware directory, builds a binary, and outputs it
@@ -69,32 +72,74 @@ func (b *Builder) Build(platform, project, commitHash string) (string, error) {
 		return "", errors.Wrap(err, "change directory")
 	}
 
+	b.l.Info("building project")
+
+	err = b.buildSequence(platform, project, commitHash)
+	if err != nil {
+		return "", errors.Wrap(err, "build project")
+	}
+
+	b.l.Info("getting binary path")
+
+	var binaryPath string
+	if platform == _platformStm32 {
+		binaryPath = b.firmwarePath + b.directoryBuilder(_buildFolder, project, platform) + project + _defaultBinaryExtension
+	} else {
+		binaryPath = b.firmwarePath + b.directoryBuilder(_buildFolder, project, platform) + _defaultBinaryName
+	}
+
+	b.l.Info("change directory to original")
+
+	err = os.Chdir(currentDir)
+	if err != nil {
+		return "", errors.Wrap(err, "change directory")
+	}
+
+	return binaryPath, nil
+}
+
+func (b *Builder) buildSequence(platform, project, commitHash string) error {
 	b.l.Info("checking out commit hash")
 
-	checkoutCmd := exec.Command(_checkoutCmd, commitHash)
-
-	_, err = checkoutCmd.CombinedOutput()
+	err := b.executeCommand(_checkoutCmd, commitHash)
 	if err != nil {
-		return "", errors.Wrap(err, "checkout commit hash")
+		return errors.Wrap(err, "checkout commit hash")
 	}
 
 	b.l.Info("updating submodules")
 
-	submoduleCmd := exec.Command(_submoduleUpdateCmd, _submoduleInitArg, _submoduleRecursiveArg)
-
-	_, err = submoduleCmd.CombinedOutput()
+	err = b.executeCommand(_submoduleUpdateCmd, _submoduleInitArg, _submoduleRecursiveArg)
 	if err != nil {
-		return "", errors.Wrap(err, "update submodules")
+		return errors.Wrap(err, "update submodules")
 	}
 
-	buildCmd := exec.Command(_makeCmd, _makePlatformArg+platform, _makeProjectArg+project)
+	b.l.Info("making binary")
 
-	_, err = buildCmd.CombinedOutput()
+	err = b.executeCommand(_makeCmd, _makePlatformArg+platform, _makeProjectArg+project)
 	if err != nil {
-		return "", errors.Wrap(err, "update submodules")
+		return errors.Wrap(err, "make binary")
 	}
 
-	//binaryPath := b.firmwarePath + _buildFolder + project + platform +
+	return nil
+}
 
-	return "", nil
+func (b *Builder) executeCommand(cmd string, args ...string) error {
+	command := exec.Command(cmd, args...)
+
+	_, err := command.CombinedOutput()
+	if err != nil {
+		return errors.Wrap(err, "execute command")
+	}
+
+	return nil
+}
+
+func (b *Builder) directoryBuilder(component ...string) string {
+	var outputPath string
+
+	for _, c := range component {
+		outputPath += c + "/"
+	}
+
+	return outputPath
 }
