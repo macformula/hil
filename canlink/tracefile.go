150da9a987e082c2448774ae754cc69b16419dd4
samparent97 <samuelparent97@gmail.com>
2024-04-24 01:00:34 -0400

    Change interface

--- canlink/tracefile.go
+++ canlink/tracefile.go
@@ -3,5 +3,5 @@ package canlink
 // TraceFile is a file that should be able to log CAN frames.
 type TraceFile interface {
 	// dumpToFile takes a list of CAN frames and writes them to a file.
-	dumpToFile([]TimestampedFrame) error
+	dumpToFile(frames []TimestampedFrame, traceDir, busName string) error
 }
