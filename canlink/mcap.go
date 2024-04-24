e194d8adbedaab0c0dce7fa4a99a8acddaf02c9e
samparent97 <samuelparent97@gmail.com>
2024-04-24 01:00:45 -0400

    Refactor ascii and mcap

--- canlink/mcap.go
+++ canlink/mcap.go
@@ -3,8 +3,9 @@ package canlink
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/macformula/hil/utils"
 	"os"
-	"strconv"
+	"time"
 
 	"github.com/foxglove/mcap/go/mcap"
 	"github.com/pkg/errors"
@@ -12,175 +13,216 @@ import (
 )
 
 const (
-	_mcap = ".mcap"
-	_json = "json"
+	_mcapLoggerName   = "mcap_file"
+	_mcap             = "mcap"
+	_json             = "json"
+	_schemaName       = "MacFormula"
+	_schemaEncoding   = "jsonschema"
+	_schemaData       = `{"type":"object"}`
+	_schemaId         = 1
+	_initialChannelId = 1
+)
+
+type (
+	// Mcap writes records to "channels". Each CAN signal gets mapped to a channelId.
+	channelId = uint16
 )
 
 // Mcap stores the information required to create each MCAP file.
 type Mcap struct {
-	l              *zap.Logger
-	file           *os.File
-	signalChannels map[string]uint16
-	client         *CANClient
-	currIndex      uint16
+	l         *zap.Logger
+	file      *os.File
+	canClient *CanClient
+
+	// Maps (msg name + signal name) to channelId
+	signalChannelIds map[string]channelId
+	nextChannelId    channelId
+
+	schema *mcap.Schema
+	header *mcap.Header
+	writer *mcap.Writer
 }
 
 // NewMcap returns a new Mcap with its own MCAP file.
-func NewMcap(l *zap.Logger, c *CANClient, dir string, busName string) (*Mcap, error) {
-	f, err := createFile(l, dir, busName, _mcap)
-	if err != nil {
-		e := errors.Wrap(err, "error creating mcap file")
-		l.Error(e.Error())
-		return nil, e
+func NewMcap(c *CanClient, l *zap.Logger) *Mcap {
+	return &Mcap{
+		l:                l.Named(_mcapLoggerName),
+		canClient:        c,
+		signalChannelIds: map[string]channelId{},
+		nextChannelId:    _initialChannelId,
+		header:           &mcap.Header{},
+		schema: &mcap.Schema{
+			ID:       _schemaId,
+			Name:     _schemaName,
+			Encoding: _schemaEncoding,
+			Data:     []byte(_schemaData),
+		},
 	}
-
-	m := &Mcap{
-		l:              l.Named("mcap_file"),
-		client:         c,
-		file:           f,
-		signalChannels: make(map[string]uint16),
-		currIndex:      1,
-	}
-
-	return m, nil
 }
 
 // dumpToFile takes a CAN frame and writes it to an MCAP file.
-func (m *Mcap) dumpToFile(s []TimestampedFrame) error {
-	// creating new instance of writer
-	w, err := mcap.NewWriter(m.file, &mcap.WriterOptions{
-		Chunked: false,
-	})
+func (m *Mcap) dumpToFile(frames []TimestampedFrame, traceDirectory, busName string) error {
+	var (
+		oneTimeErr        = utils.ResettableError{}
+		mcapWriterOptions = &mcap.WriterOptions{
+			Chunked: false,
+		}
+	)
+
+	// Creating trace file.
+	file, err := createTraceFile(traceDirectory, busName, _mcap)
 	if err != nil {
-		m.l.Info("error creating writer")
+		m.l.Error("failed to create trace file", zap.Error(err))
+
+		return errors.Wrapf(err, "create trace file (%s)", m.file.Name())
+	}
+
+	// Creating new instance of writer. Must do this every time.
+	m.writer, err = mcap.NewWriter(file, mcapWriterOptions)
+	if err != nil {
+		m.l.Error("failed creating mcap writer", zap.Error(err))
+
 		return errors.Wrap(err, "error creating writer")
 	}
 
-	defer func() {
-		err = m.closeWriter(w)
-		if err != nil {
-			m.l.Info("error closing writer")
-			return
-		}
-	}()
-
-	err = m.initHeaders(err, w)
+	// Writing headers.
+	err = m.writer.WriteHeader(m.header)
 	if err != nil {
-		m.l.Info("error initializing header")
-		return errors.Wrap(err, "error initializing header")
+		m.l.Error("failed to write header", zap.Error(err))
+
+		oneTimeErr.Set(errors.Wrap(err, "error creating headers"))
 	}
 
-	for _, value := range s {
-		err = m.writeMessage(w, value)
+	// Writing schema.
+	err = m.writer.WriteSchema(m.schema)
+	if err != nil {
+		m.l.Error("failed to write schemas", zap.Error(err))
+
+		oneTimeErr.Set(errors.Wrap(err, "error creating schema"))
+	}
+
+	// Write all timestamped frames to file.
+	for i := 0; i < len(frames); i++ {
+		err = m.writeCanMessage(&frames[i])
 		if err != nil {
-			m.l.Info("error writing message")
-			return errors.Wrap(err, "error writing message")
+			m.l.Info("error writing can message")
+
+			oneTimeErr.Set(errors.Wrap(err, "error writing message"))
+		}
+	}
+
+	// Closing writer.
+	err = m.writer.Close()
+	if err != nil {
+		m.l.Error("failed to close writer", zap.Error(err))
+
+		return errors.Wrap(err, "close writer")
+	}
+
+	return nil
+}
+
+// writeCanMessage loops through all the signals and calls writeCanSignal.
+func (m *Mcap) writeCanMessage(timestampedFrame *TimestampedFrame) error {
+	frame, err := m.canClient.UnmarshalFrame(timestampedFrame.Frame)
+	if isIdNotInDatabaseError(err) {
+		// This frame id does not exist in the dbc
+		return nil
+	} else if err != nil {
+		m.l.Error("failed to unmarshal frame", zap.Error(err))
+
+		return errors.Wrap(err, "unmarshal frame")
+	}
+
+	msg := frame.Descriptor()
+	signals := msg.Signals
+	msgData := frame.Frame().Data
+
+	for _, sig := range signals {
+		sigValue := sig.UnmarshalPhysical(msgData)
+
+		err = m.writeCanSignal(msg.Name, sig.Name, sigValue, timestampedFrame.Time)
+		if err != nil {
+			m.l.Error("failed to write can signal", zap.Error(err))
+
+			return errors.Wrap(err, "write can signal")
 		}
 	}
 
 	return nil
 }
 
-// writeMessage loops through all the signals and calls writeSignal.
-func (m *Mcap) writeMessage(w *mcap.Writer, value TimestampedFrame) error {
-	//checking map to see if channel for the signalID is already created
-	frame, err := m.client.UnmarshalFrame(value.Frame)
+// writeCanSignal creates the Mcap channel if it doesn't exist and then writes the CAN Message to it.
+func (m *Mcap) writeCanSignal(msgName, sigName string, signalValue float64, receivedTime time.Time) error {
+	chanId, err := m.getChannelId(msgName, sigName)
+
+	signalValueJson, err := json.Marshal(signalValue)
 	if err != nil {
-		m.l.Error(err.Error())
-		return errors.Wrap(err, "error unmarshalling frame using canclient")
-	} else {
-		messageName := frame.Descriptor().Name
-		for _, signal := range frame.Descriptor().Signals {
-			err = m.writeSignal(w, value, messageName, signal.Name, signal.UnmarshalPhysical(value.Frame.Data))
-			if err != nil {
-				m.l.Info("error writing signal")
-				return errors.Wrap(err, "error writing signal")
-			}
-		}
+		m.l.Info("failed to marshal message into json", zap.Error(err))
+
+		return errors.Wrap(err, "json marshal")
+	}
+
+	timeReceivedUnix := uint64(receivedTime.UnixNano())
+	mcapMsg := mcap.Message{
+		ChannelID:   chanId,
+		LogTime:     timeReceivedUnix, // Time at which the message was recorded.
+		PublishTime: timeReceivedUnix, // Time at which the message was published. Not available, must set to log time.
+		Data:        signalValueJson,
+	}
+
+	err = m.writer.WriteMessage(&mcapMsg)
+	if err != nil {
+		m.l.Error(
+			"failed to write mcap message",
+			zap.Uint16("channel_id", chanId),
+			zap.Uint64("receive_time", timeReceivedUnix),
+			zap.Error(err),
+		)
+
+		return errors.Wrap(err, "write message")
 	}
 
 	return nil
 }
 
-// writeSignal creates the Mcap channel if it doesn't exist and then writes the CAN Message to it.
-func (m *Mcap) writeSignal(w *mcap.Writer, value TimestampedFrame, messageName string, signalName string, signalValue float64) error {
-	signalID := uint16(value.Frame.ID)
-	var channelID uint16
-	if val, exists := m.signalChannels[strconv.Itoa(int(signalID))+"."+signalName]; exists {
-		channelID = val
-	} else {
-		m.l.Info("channel created")
-		channelID = m.currIndex
-		m.currIndex++
-		m.signalChannels[strconv.Itoa(int(signalID))+"."+signalName] = channelID
-		err := w.WriteChannel(&mcap.Channel{
-			ID:              channelID,
-			Topic:           messageName + "." + signalName,
-			MessageEncoding: _json,
-			SchemaID:        1,
-		})
+func (m *Mcap) getChannelId(msgName, sigName string) (channelId, error) {
+	chanId, ok := m.signalChannelIds[msgName+sigName]
+	if !ok {
+		chanId = m.nextChannelId
+		m.signalChannelIds[sigName] = chanId
+		// Increment channel id for next added signal.
+		m.nextChannelId++
+
+		// Channel does not yet exist, must assign a new id, then create a channel.
+		m.l.Debug("creating channel",
+			zap.String("message_name", msgName),
+			zap.String("signal_name", sigName),
+			zap.Uint16("channel_id", chanId),
+		)
+
+		err := m.createChannel(msgName, sigName, chanId)
 		if err != nil {
-			e := errors.Wrap(err, "error creating channel")
-			m.l.Error(e.Error())
-			return e
+			return 0, errors.Wrap(err, "create channel")
 		}
 	}
 
-	message, err := json.Marshal(signalValue)
-	if err != nil {
-		e := errors.Wrap(err, "error marshalling message data into json format")
-		m.l.Error(e.Error())
-		return e
-	}
-
-	t := uint64(value.Time.UnixNano())
-
-	err = w.WriteMessage(&mcap.Message{
-		ChannelID:   channelID,
-		LogTime:     t, // Time at which the message was recorded.
-		PublishTime: t, // Time at which the message was published. If not available, must be set to the log time.
-		Data:        message,
-	})
-	if err != nil {
-		m.l.Info(fmt.Sprintf("Failed to write MCAP message: ChannelID: %d, Logtime: %d", signalID, t))
-		return errors.Wrap(err, "failed to write MCAP message")
-	}
-
-	return nil
+	return chanId, nil
 }
 
-func (m *Mcap) initHeaders(err error, w *mcap.Writer) error {
-	// creating header
-	err = w.WriteHeader(&mcap.Header{})
-	if err != nil {
-		m.l.Info("error creating headers")
-		return errors.Wrap(err, "error creating headers")
+func (m *Mcap) createChannel(msgName, sigName string, chanId channelId) error {
+	mcapChannel := mcap.Channel{
+		ID:              chanId,
+		Topic:           fmt.Sprintf("%s.%s", msgName, sigName),
+		MessageEncoding: _json,
+		SchemaID:        _schemaId,
 	}
 
-	// creating the schema
-	m.l.Info("schema created")
-	err = w.WriteSchema(&mcap.Schema{
-		ID:       1,
-		Name:     "MacFormula",
-		Encoding: "jsonschema",
-		Data:     []byte(`{"type":"object"}`),
-	})
+	err := m.writer.WriteChannel(&mcapChannel)
 	if err != nil {
-		m.l.Info("error creating schemas")
-		return errors.Wrap(err, "error creating schema")
-	}
+		m.l.Error("failed to write channel", zap.Error(err))
 
-	return err
-}
-
-// closeWriter closes the Mcap Writer and write the footer
-func (m *Mcap) closeWriter(w *mcap.Writer) error {
-	err := w.Close()
-	m.l.Info("MCAP: Mcap file is closing and writer turned off ")
-	if err != nil {
-		e := errors.Wrap(err, "Error closing mcap file")
-		m.l.Error(e.Error())
-		return e
+		return errors.Wrap(err, "write channel")
 	}
 
 	return nil
